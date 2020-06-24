78b8c50f3c9f64fc93856ac0adfdd156a25101f9
Ankit Kumar Singh <chhotu12ank@gmail.com>
2020-06-24 09:33:00 +0530

    Promise Part1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -9,15 +9,15 @@ export class DishService {
 
   constructor() { }
 
-  getDishes() : Dish[] {
-    return DISHES;
+  getDishes() : Promise<Dish[]> {
+    return Promise.resolve(DISHES);
   }
 
-  getDish(id : string) : Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];  //Array function => , shorthand for normal js function
+  getDish(id : string) : Promise<Dish> {
+    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);  //Array function => , shorthand for normal js function
   }
 
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getFeaturedDish(): Promise<Dish> {
+    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
   }
 }
