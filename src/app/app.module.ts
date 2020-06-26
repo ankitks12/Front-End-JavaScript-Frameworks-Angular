89f2675069271ebfd9b25083f508df427e4617d5
Ankit Kumar Singh <chhotu12ank@gmail.com>
2020-06-26 19:45:04 +0530

    Assignment 3 Donegit add .git add .git add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
@@ -65,7 +66,8 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [
     DishService,
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 import { from } from 'rxjs';
 
 @Component({
@@ -18,9 +20,33 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
 
+  commentForm: FormGroup;
+  cmt: Comment;
+
+  @ViewChild('fform') feedbackFormDirective;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be atleast 2 characters long',
+      'maxlength': 'Name cannot be more than 25 characters long'
+    },
+    'comment' : {
+      'required': 'Comment is required'
+    }
+  };
+
   constructor(private dishService : DishService,
     private route: ActivatedRoute,
-    private location : Location) { }
+    private location : Location,
+    private cb: FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -38,4 +64,59 @@ export class DishdetailComponent implements OnInit {
     this.location.back();
   }
 
+  createForm() {
+    this.commentForm = this.cb.group({
+      rating: '5',
+      author : ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      comment: ['', Validators.required],
+      date: new Date().toISOString()
+    });
+
+    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set form validation messages
+  }
+
+  onValueChanged(data?: any) {
+    if(!this.commentForm) { return; }
+    const form = this.commentForm;
+    for(const field in this.formErrors) {
+      if(this.formErrors.hasOwnProperty(field)){
+        // clear previous error messages (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid){
+          const messages = this.validationMessages[field];
+          for(const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  rating:number;
+
+  onSubmit() {
+    var d = new Date();
+    var da = d.toISOString();
+    this.commentForm.value.date = da;
+    this.commentForm.value.rating = this.rating;
+    this.cmt = this.commentForm.value;
+    console.log(this.cmt);
+    this.dish.comments.push(this.commentForm.value);
+    this.commentForm.reset({
+      rating: '5',
+      author: '',
+      comment: '',
+      date: ''
+    });
+    this.feedbackFormDirective.resetForm();
+  }
+
+  pitch(event: any) {
+    this.rating = event.value;
+  }
 }
